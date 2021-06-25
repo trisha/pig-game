33adfe47ae8f787ae4b55833364300f9cc3c6335
Your Name <you@example.com>
2021-06-25 15:18:30 -0700

    Add message for dice results. Add game logic as well as Hold button. Next step: Add Hold logic, update display upon Win condition.

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,28 +10,52 @@ const Game = (props) => {
     var [firstDie, setFirstDie] = useState(Math.floor(Math.random() * 6) + 1)
     var [secondDie, setSecondDie] = useState(Math.floor(Math.random() * 6) + 1)
     var [message, setMessage] = useState('')
-    var [refreshFlag, setRefreshFlag] = useState(false) // Can probably use currentPlayer as a refresh flag.
+    var [secondMessage, setSecondMessage] = useState('')
+    var [choose, setChoose] = useState(false)
+    var [winner, setWinner] = useState(false)
+    // var [refreshFlag, setRefreshFlag] = useState(false) // Can probably use currentPlayer as a refresh flag.
     
-    // Have to use the below so that the dice values update right after rolling.
-    useEffect(() => {
-    }, [refreshFlag])
+    // // Have to use the below so that the dice values update right after rolling.
+    // useEffect(() => {
+    // }, [refreshFlag])
+
+    const nextTurn = () => {
+        setCurrentPlayer((currentPlayer + 1) % props.players.length)
+    }
+
+    const checkWin = () => {
+        if (props.players[currentPlayer].totalScore >= 100) {
+            setWinner(true)
+        }
+    }
 
     const handleDiceLogic = (first, second) => {
         let newPlayersList = props.players
         setMessage('')
+        setSecondMessage('')
+        setChoose(false)
         if (first === 1 && second === 1) {
             newPlayersList[currentPlayer].turnScore = 0
             newPlayersList[currentPlayer].totalScore = 0
-            setMessage("You rolled two 1's, which means your total score goes to 0 and it's the next player's turn!")
-            setCurrentPlayer((currentPlayer + 1) % props.players.length)
+            setMessage(`You rolled two 1's, which means your total score goes to 0 and it's now ${props.players[(currentPlayer + 1) % props.players.length].name}'s turn!`)
+            nextTurn()
         } else if (first === 1 || second === 1) {
-            setMessage("You rolled exactly one 1, which means nothing gets added to your total score and it's the next player's turn!")
-            setCurrentPlayer((currentPlayer + 1) % props.players.length)
+            setMessage(`You rolled exactly one 1, which means nothing gets added to your total score and it's now ${props.players[(currentPlayer + 1) % props.players.length].name}'s turn!`)
+            newPlayersList[currentPlayer].turnScore = 0
+            nextTurn()
+        } else if (first === second) {
+            checkWin()
+            setMessage("Because you got two matching numbers (that aren't 1's), choose to either continue rolling, or hold.")
+            setSecondMessage("Holding will end your turn but add your turn score to your total score.")
+            setChoose(true)
+            newPlayersList[currentPlayer].turnScore += (first + second)
+        } else {
+            setMessage(`${first + second} was added to your turn score. Continue rolling.`)
+            newPlayersList[currentPlayer].turnScore += (first + second)
         }
-        
-
-        newPlayersList[currentPlayer].turnScore += (first + second)
         props.setPlayers(newPlayersList)
+
+        
     }
 
     const rollDice = (e) => {
@@ -43,6 +67,7 @@ const Game = (props) => {
         setSecondDie(second)
     }
 
+    
     return (
         <div>
             <p>Game Component</p>
@@ -74,8 +99,17 @@ const Game = (props) => {
             <hr />
             < Die value={firstDie} />
             < Die value={secondDie} />
-            {message}
-            <Button style={{display: 'block'}} variant="success" onClick={(e)=>rollDice(e)}>Roll the Dice!</Button>
+            <br />
+            <div>
+                <b>{message}</b>
+                <br />
+                <b>{secondMessage}</b>
+            </div>
+            <br />
+            <div>
+                <Button style={{display: 'inline-block', marginRight: '10px'}} variant="success" onClick={(e)=>rollDice(e)}>Roll the Dice!</Button>
+                {choose ? <Button style={{display: 'inline-block'}} variant="info">Hold</Button> : <div></div>}
+            </div>
             
             <hr />
             <h3>How to Play Pig</h3>
